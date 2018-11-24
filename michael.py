fb4ad7ddae84222820d8f1c5c771abc2dc32fba3
Michael Ellsworth <102984@isp.cz>
2018-11-24 09:05:59 +0100

    remeber to put in a message

--- michael.py
+++ michael.py
@@ -1,5 +1,21 @@
 import sys
 from PyQt5 import QtWidgets
 
-# everyone just code in your own files and we will combine
-# them later into the app file
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    l = QtWidgets.QLabel(w)
+    b = QtWidgets.QPushButton(w)
+    v = QtWidgets.QVBoxLayout()
+    v.addWidget(b)
+    v.addWidget(l)
+    w.setLayout(v)
+    b.setText('Don\'t push me')
+    l.setText('Hello World')
+    w.setGeometry(100,100, 400, 400)
+    w.setWindowTitle('SIW Prototype')
+    w.show()
+    sys.exit(app.exec_())
+
+
+window()
